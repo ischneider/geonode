0c26015f1cdd2881ed20eef1a5b052e3bb9ae8cd
Ian Schneider <planablediglet@gmail.com>
2012-06-12 15:20:49 -0600

    pass data directory to jetty if environment variable is passed

--- geoserver-geonode-ext/startup.sh
+++ geoserver-geonode-ext/startup.sh
@@ -1,1 +1,5 @@
-MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=256m -XX:CompileCommand=exclude,net/sf/saxon/event/ReceivingContentHandler.startElement" mvn jetty:run
+#!/bin/bash
+if [ $GS_DATA != '' ]; then
+  DATA_DIR="-DGEOSERVER_DATA_DIR=$GS_DATA"
+fi
+MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=256m -XX:CompileCommand=exclude,net/sf/saxon/event/ReceivingContentHandler.startElement" mvn jetty:run $DATA_DIR
